02b1af990cf3aad1fb9a9a211ea0c1dd7c3a0f01
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2023-05-27 00:52:18 +0600

    App: test example

--- anvil/app/main.cpp
+++ anvil/app/main.cpp
@@ -1,10 +1,28 @@
 
+#include "SDL3/SDL.h"
 #include <iostream>
 
-using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        640,
+        480,
+        0
+        );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, " ", 0);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
