0beb9097d45cbd4f42c572bb825f2ec3bfe4ab36
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2023-05-29 21:34:21 +0600

    Project: fix layout in cmake

--- src/anvil/window.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "window.h"
-
-Window::Window(const std::string &title, int width, int height, SDL_WindowFlags flags)
-    : m_window(SDL_CreateWindow(title.c_str(), width, height, flags), &SDL_DestroyWindow)
-{
-    SDL_assert(m_window);
-}
