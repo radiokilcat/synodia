c36eb77128c8699b62480f50de0b3ef8895ce379
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2023-07-06 12:33:11 +0600

    Add game states

---
+++ anvil/include/gamestate.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "gameobject.h"
+
+namespace anvil {
+
+class GameState
+{
+public:
+    virtual bool onEnter() = 0;
+    virtual bool onExit() = 0;
+
+    virtual void update() = 0;
+    virtual void render() = 0;
+
+    virtual std::string getID() = 0;
+};
+
+}
