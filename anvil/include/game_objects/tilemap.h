db9eaea05b298358e1a942770bbd6fd6a1e4ceba
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2023-07-26 11:41:50 +0600

    Separate Tiles from TileMap
    load tiles from the start and save them to map

--- anvil/include/game_objects/tilemap.h
+++ anvil/include/game_objects/tilemap.h
@@ -4,20 +4,48 @@
 
 namespace anvil {
 
-class TileMap: public GameObject
+class Tile : GameObject {
+public:
+    Tile(LoaderParams* params);
+    Tile() {};
+    void draw();
+    void update();
+    void clean();
+
+    int getScreenX();
+    int getScreenY();
+    int getWidth();
+    int getHeight();
+
+private:
+    bool outlined = false;
+};
+
+class TileMap : public GameObject
 {
 public:
     TileMap();
     void draw_tile(std::string id, int x, int y, std::shared_ptr<Renderer> renderer);
     void draw(std::shared_ptr<Renderer> renderer) override;
     void clean() override;
+    void update() override;
+
     void from_json(nlohmann::json& j) override;
     void to_json(nlohmann::json& j) override;
+
+    void loadTiles();
+
+    std::pair<int, int> findScreenPosition(std::string textureId, int x, int y);
+    std::pair<int, int> getTileByPosition(float x, float y, std::shared_ptr<Renderer> renderer) ;
+
     ~TileMap() {};
     static bool registerWithFactory();
 private:
     std::vector<std::vector<std::string>> m_grid;
 
+    void outline_tile(int x, int y, std::shared_ptr<Renderer> renderer);
+
+    std::map<std::pair<int, int>, Tile> m_tiles;
     int m_tileWidth;
     int m_tileHeight;
 
