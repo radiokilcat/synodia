777e261535f833a9cade441dbf5187a524d13818
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2023-09-04 21:28:31 +0600

    Fix visual studio append Release/Debug to build path

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -1,17 +1,11 @@
 set(ASSET_SOURCE_DIR "${CMAKE_SOURCE_DIR}/apps/test_app/res")
 
-set(ASSET_DESTINATION_DIR "${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}")
+set(ASSET_DESTINATION_DIR "${CMAKE_BINARY_DIR}/bin/")
 
 
 set(SETTINGS_SOURCE ${CMAKE_SOURCE_DIR}/apps/test_app/settings.ini)
-set(SETTINGS_DESTINATION "${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}")
-# TODO: IN MSVC CMAKE_CFG_INTDIR won't be resolved into config name
-# Seems like resoures must be included in the solution and be copied
-# in compile time
-if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-	set(ASSET_DESTINATION_DIR "${CMAKE_BINARY_DIR}/bin/Debug")
-	set(SETTINGS_DESTINATION "${CMAKE_BINARY_DIR}/bin/Debug")
-endif()
+set(SETTINGS_DESTINATION "${CMAKE_BINARY_DIR}/bin/")
+
 file(COPY ${ASSET_SOURCE_DIR} DESTINATION ${ASSET_DESTINATION_DIR})
 configure_file(${SETTINGS_SOURCE} ${SETTINGS_DESTINATION}/settings.ini COPYONLY)
 
