b7141f9c2aa2fa267b0bd3a67b418a0f9f9b4507
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2024-10-17 21:26:18 +0500

    StateLoader: re-organize assets and loading

--- anvil/include/json_serializer/stateloader.h
+++ anvil/include/json_serializer/stateloader.h
@@ -18,7 +18,9 @@ public:
     void operator=(StateLoader const&) = delete;
 
     static StateLoader& instance();
+    static std::filesystem::path getTexturesConfigFile();
     static std::filesystem::path getStateConfigFile(const std::string& stateId);
+    static std::filesystem::path getAudioConfigFile();
     static std::vector<std::string> loadTextures(const std::string& stateId);
     static void loadAudio(const std::string& stateId);
     static void addTransformComponent(std::shared_ptr<IGameObject>& root, const json& data);
@@ -30,9 +32,9 @@ public:
     static std::shared_ptr<IGameObject> loadGameObjects(const std::string& stateId);
     static std::shared_ptr<IGameObject> loadMenu(const std::string& stateId);
     static nlohmann::json loadObjectTemplate(std::shared_ptr<IGameObject>& object, const nlohmann::json& data);
+    static void loadUI(const std::shared_ptr<IGameObject>& root, const json& data);
     static std::shared_ptr<IGameObject> loadObjectAndChildren(const json& rootData);
     static void clearTextures(const std::string& stateId);
-    static std::filesystem::path getConfigFile();
 
 private:
     StateLoader() {}
