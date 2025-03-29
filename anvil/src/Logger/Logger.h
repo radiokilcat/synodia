f73b5562955f204ad9aa821f0b5a10228962d0bf
Aleksandr Plaksin <aleksandr.plaksin@mytona.com>
2025-03-29 20:28:10 +0500

    Logger: make formatted ouput

--- anvil/src/Logger/Logger.h
+++ anvil/src/Logger/Logger.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <fmt/format.h>
 
 namespace anvil {
 
@@ -19,8 +20,29 @@ struct LogEntry {
 class Logger {
     public:
         static std::vector<LogEntry> messages;
-        static void Log(const std::string& message);
-        static void Err(const std::string& message);
-};
+    
+        template <typename... Args>
+        static void Log(fmt::format_string<Args...> fmtStr, Args&&... args) {
+            LogImpl(fmt::format(fmtStr, std::forward<Args>(args)...));
+        }
+    
+        template <typename... Args>
+        static void Err(fmt::format_string<Args...> fmtStr, Args&&... args) {
+            ErrImpl(fmt::format(fmtStr, std::forward<Args>(args)...));
+        }
+
+        // Legacy-style
+        static void Log(const std::string& message) {
+            LogImpl(message);
+        }
+
+        static void Err(const std::string& message) {
+            LogImpl(message);
+        }
+    
+    private:
+        static void LogImpl(const std::string& message);
+        static void ErrImpl(const std::string& message);
+    };
 
 }
