f091e6dfd160edf935bf90f29f342f7bd542fdc8
Alexander Plaksin <plaksin93ao@gmail.com>
2026-06-02 22:32:37 +0500

    CMake: fix deploy scripts

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -5,3 +5,39 @@ function(copy_assets TARGET_NAME SOURCE_ASSETS SOURCE_CONFIG)
         COMMENT "Copying assets for ${TARGET_NAME}..."
     )
 endfunction()
+
+function(copy_anvil_dlls TARGET_NAME)
+    if(NOT BUILD_SHARED_LIBS)
+        return()
+    endif()
+    add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/anvil.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/SDL3.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/SDL3_image.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/SDL3_ttf.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/SDL3_mixer.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/fmt$<$<CONFIG:Debug>:d>.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/libwebp.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/libwebpdemux.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/bin/$<CONFIG>/libwebpmux.dll
+            $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMENT "Copying DLLs for ${TARGET_NAME}..."
+    )
+endfunction()
